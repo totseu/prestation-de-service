f7eeefb93e6c6c6900e739770ff5c7b08d5a19c5
Nathan Totseu <totseunathan@gmail.com>
2026-06-22 05:04:31 +0100

    base de donnee

--- config/settings.py
+++ config/settings.py
@@ -95,10 +95,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 import os
 import dj_database_url
 
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
